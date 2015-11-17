9563e70b8dcf37cdd093d89a60cdf1914bd7f885
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-11-16 20:13:43 -0700

    Clean up and working UI for login

--- www/js/app.js
+++ www/js/app.js
@@ -99,44 +99,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
         }
     })
 
-
-
-    //    .state('app.search', {
-    //        url: '/search',
-    //        views: {
-    //            'menuContent': {
-    //                templateUrl: 'templates/search.html'
-    //            }
-    //        }
-    //    })
-    //
-    //    .state('app.browse', {
-    //            url: '/browse',
-    //            views: {
-    //                'menuContent': {
-    //                    templateUrl: 'templates/browse.html'
-    //                }
-    //            }
-    //        })
-    //        .state('app.playlists', {
-    //            url: '/playlists',
-    //            views: {
-    //                'menuContent': {
-    //                    templateUrl: 'templates/playlists.html',
-    //                    controller: 'PlaylistsCtrl'
-    //                }
-    //            }
-    //        })
-    //
-    //    .state('app.single', {
-    //        url: '/playlists/:playlistId',
-    //        views: {
-    //            'menuContent': {
-    //                templateUrl: 'templates/playlist.html',
-    //                controller: 'PlaylistCtrl'
-    //            }
-    //        }
-    //    });
-    // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/home');
 });
